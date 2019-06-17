6825bfb0d2ff7943be48f8ef5dd7b7dece9de5bb
Gleidson Duarte <gleidson.duarte@hotmail.com.br>
2019-06-17 18:01:27 -0300

    Décima primeira aula explicando sobre como fazer um formulário simples com Angular

--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { UsuarioComponent } from './usuario.component';
 import { UsuarioRoutingModule } from './usuario.routing.module';
+import { UsuarioCadastroComponent } from './cadastro/usuario-cadastro.component';
 
 @NgModule({
-  declarations: [UsuarioComponent],
+  declarations: [UsuarioComponent, UsuarioCadastroComponent],
   imports: [
     CommonModule,
-    UsuarioRoutingModule
+    UsuarioRoutingModule,
+    FormsModule
   ]
 })
 export class UsuarioModule { }
--- src/app/usuario/usuario.routing.module.ts
+++ src/app/usuario/usuario.routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { UsuarioComponent } from './usuario.component';
+import { UsuarioCadastroComponent } from './cadastro/usuario-cadastro.component'
 
 const routes: Routes = [
   {
   	path: '',
   	component: UsuarioComponent
+  },
+  {
+  	path: 'add',
+  	component: UsuarioCadastroComponent
   }
 ];
 
